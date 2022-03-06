0dd14abf07eefb2c34d3e94d4eac3c10663eedc2
SAKSHAM <sakshaa1@uci.edu>
2022-03-06 15:47:45 -0800

    connect_form update

--- frontend/connect_form.js
+++ frontend/connect_form.js
@@ -1,22 +1,31 @@
 
-function myfunction(){
+const axios = require('axios');
+
+function makeGetRequest(){
     var element_id = document.getElementById("uci_netid").value;
 	var element_fname = document.getElementById("first_name").value;
 	var element_lname = document.getElementById("last_name").value;
 	var element_g = document.getElementById("gender").value;
 	var element_d = document.getElementById("disability").value;
 	var element_year = document.getElementById("year").value;
+	var element_major = document.getElementById("major").value;
+	var element_v = document.getElementById("veteran").value;
+	var element_we = document.getElementById("work").value;
+	var element_skills = document.getElementById("skills").value;
+	var element_wa = document.getElementById("work_auth").value;
+	var element_race = document.getElementById("race_dropdown_container").value;
+	
     var base ='http://127.0.0.1:5000/profile/'
-    axios.get(base+element_id&&element_d&&element_fname&&element_lname&&element_g&&element_year).then(
-    (response) => {
-            var result = response.data;
-            var num= Object.keys(result).length;
-            if (num>0){
-                window.location.href="profile.html"
-            } else{
-                window.location.href="form.html"
-            }
-        },
-
-    );
+	uci_netid, first_name, last_name, major, year, gender, sex, disability,
+                     veteran, work_ex, skills, work_auth, grad_date, ethinicity,
+                     email, pass_bool
+    let payload = { uci_netid : element_id, first_name : element_fname, last_name: element_lname, major: element_major, year : element_year, gender : element_g, sex : element_g, disability : element_d,
+                     veteran : element_v, work_ex : element_we, skills : element_skills, work_auth : element_wa, grad_date : element_year, ethinicity : element_race};
+	let res = await axios.post('http://127.0.0.1:5000/profile/', payload);
+	
+	let data = res.data;
+    console.log(data);
 }
+makeGetRequest();
+
+
