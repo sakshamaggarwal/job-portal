abd2013e1e51353ee712c3851c056a5ebf1275d5
Natasha Kulkarni <74796537+natk27@users.noreply.github.com>
2022-03-06 20:56:15 -0800

    Added Error 500 condition

--- frontend/connect_login.js
+++ frontend/connect_login.js
@@ -2,16 +2,16 @@
 function myfunction(){
     var element = document.getElementById("txtuser").value;
     var base ='http://127.0.0.1:5000/profile/'
-    axios.get(base+element).then(
-    (response) => {
+    axios.get(base+element)
+    .then((response) => {
             var result = response.data;
-            var num= Object.keys(result).length;
-            if (num>0){
-                window.location.href="profile.html"
-            } else{
-                window.location.href="form.html"
-            }
-        },
+            window.location.href="profile.html"
 
-    );
-}
+        }).catch(function (error) {
+    if (error.response.status==500) {
+
+      console.log(error.response.status);
+      window.location.href="form.html"
+    }
+
+});
