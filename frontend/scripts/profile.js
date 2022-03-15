43d059d7598d380a1d73b109e6149391ad6c317d
Nirmit Shetty <nirmit123@gmail.com>
2022-03-15 16:31:10 -0700

    static profile page edited

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -1,22 +1,44 @@
-function edit()
+function formAction()
 {
+  //console.log("form submitted");
   btn=document.getElementById('editbtn');
-
   if(btn.innerText=="Edit")
   {
     btn.innerText="Save";
-    document.getElementById('cancelbtn').style.visibility='visible';
-    changeReadOnly(false);
+    edit();
   }
-  else {
-    document.getElementById('cancelbtn').style.visibility='hidden';
+  else
+  {
     btn.innerText="Edit";
-    changeReadOnly(true);
+    save();
   }
+}
+
+function edit()
+{
+  //console.log("edit called");
+  document.getElementById('cancelbtn').style.visibility='visible';
+  changeReadOnly(false);
+}
+
+function save()
+{
+  //console.log("save called");
+
+  /*
+  add save logic
+  */
+
+  document.getElementById('cancelbtn').style.visibility='hidden';
+  changeReadOnly(true);
 }
 
 function cancel()
 {
+  /*
+  add change logic
+  */
+
   btn=document.getElementById('editbtn');
   btn.innerText="Edit";
   document.getElementById('cancelbtn').style.visibility='hidden';
